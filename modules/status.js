1e03f0406394ba428a8a159f7ab6949c18a6141f
danielps1 <daniel.pasch.s@gmail.com>
2015-11-20 22:06:36 +0100

    fixed filesizes

--- modules/status.js
+++ modules/status.js
@@ -17,20 +17,19 @@ client.on('chat', function (channel, user, message, self) {
 	var messageStart = message.substr(0,12).toLowerCase();
     if (messageStart === '!status logs' && global.cooldown === false) {
 		var name = fn.getNthWord(message, 3).toLowerCase();
-		var fileSize = 0;
 		console.log(name);
 
 
 		if (name === 'channel') {
 			if ((fn.getFilesizeInKilobytes('logs/' + channel.substr(1) + '.txt').toFixed(2) + ' KB') > 1000) {
-				fileSize = fn.getFilesizeInMegabytes('logs/' + channel.substr(1) + '.txt').toFixed(2) + ' MB';
+				var fileSize = fn.getFilesizeInMegabytes('logs/' + channel.substr(1) + '.txt').toFixed(2) + ' MB';
 			}
 			client.say(channel, '@' + user['username'] + ', ' + 'log file for channel ' + channel.substr(1) + ' is ' + fileSize);
         	global.cooldown = true;
 		}
 		else {
 			if ((fn.getFilesizeInKilobytes('logs/' + channel.substr(1) + '/' + name +  '.txt').toFixed(2) + ' KB') > 1000) {
-				fileSize = fn.getFilesizeInMegabytes('logs/' + channel.substr(1) + '/' + name +  '.txt').toFixed(2) + ' MB';
+				var fileSize = fn.getFilesizeInMegabytes('logs/' + channel.substr(1) + '/' + name +  '.txt').toFixed(2) + ' MB';
 			}
 			client.say(channel, '@' + user['username'] + ', ' + 'log file for ' + name + ' is ' + fileSize);
 			global.cooldown = true;
