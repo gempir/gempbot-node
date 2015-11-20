7b33f5648078377a19fe14069a026b1588c03db0
danielps1 <daniel.pasch.s@gmail.com>
2015-11-20 22:21:14 +0100

    mb and kb should now show up correctly

--- modules/status.js
+++ modules/status.js
@@ -20,19 +20,23 @@ client.on('chat', function (channel, user, message, self) {
 		console.log(name);
 
 		if (name === 'channel') {
-			var fileSize = fn.getFilesizeInKilobytes('logs/' + channel.substr(1) + '.txt').toFixed(0) + ' KB';
+			var fileSize = fn.getFilesizeInKilobytes('logs/' + channel.substr(1) + '.txt').toFixed(0);
+			var extension = ' KB';
 			if (fileSize > 1000) {
-				fileSize = fn.getFilesizeInMegabytes('logs/' + channel.substr(1) + '.txt').toFixed(2) + ' MB';
+				fileSize = fn.getFilesizeInMegabytes('logs/' + channel.substr(1) + '.txt').toFixed(2);
+				extension = ' MB';
 			}
-			client.say(channel, '@' + user['username'] + ', ' + 'log file for channel ' + channel.substr(1) + ' is ' + fileSize);
+			client.say(channel, '@' + user['username'] + ', ' + 'log file for channel ' + channel.substr(1) + ' is ' + fileSize + extension);
         	global.cooldown = true;
 		}
 		else {
-			var fileSize = fn.getFilesizeInKilobytes('logs/' + channel.substr(1) + '/' + name +  '.txt').toFixed(0) + ' KB';
+			var fileSize = fn.getFilesizeInKilobytes('logs/' + channel.substr(1) + '/' + name +  '.txt').toFixed(0);
+			var extension = ' KB';
 			if (fileSize > 1000) {
-				fileSize = fn.getFilesizeInMegabytes('logs/' + channel.substr(1) + '/' + name +  '.txt').toFixed(2) + ' MB';
+				fileSize = fn.getFilesizeInMegabytes('logs/' + channel.substr(1) + '/' + name +  '.txt').toFixed(2);
+				extension = ' MB';
 			}
-			client.say(channel, '@' + user['username'] + ', ' + 'log file for ' + name + ' is ' + fileSize);
+			client.say(channel, '@' + user['username'] + ', ' + 'log file for ' + name + ' is ' + fileSize + extension);
 			global.cooldown = true;
 		}
     }
