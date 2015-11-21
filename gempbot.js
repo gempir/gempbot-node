a5dc6d6c2c6d2391ed715e195bd6879fb8829332
danielps1 <daniel.pasch.s@gmail.com>
2015-11-22 00:01:46 +0100

    combo counter added

--- gempbot.js
+++ gempbot.js
@@ -2,4 +2,5 @@ var twitch = require('./modules/twitch');
 require('./modules/logs');
 require('./modules/status');
 require('./modules/count');
+require('./modules/combo');
 
---
+++ modules/combo.js
@@ -0,0 +1,21 @@
+var twitch = require('./twitch');
+var fn = require('./functions');
+
+client = twitch.client;
+
+var lastMessage = null;
+var currentMessage = null;
+var counter = 1;
+
+client.on('chat', function(channel, user, message, self) {
+    var currentMessage = message;
+
+
+    if (currentMessage != lastMessage && counter > 2 && global.cooldown === false) {
+    	client.action(channel, counter + 'x ' + lastMessage + ' COMBO');
+    }
+    if (currentMessage === lastMessage) {
+    	counter++;
+    }
+    lastMessage = message;
+});
