49719978b0257d66b8569640a2551917496e650a
danielps1 <daniel.pasch.s@gmail.com>
2015-11-22 09:25:58 +0100

    combo re-enabled

--- gempbot.js
+++ gempbot.js
@@ -2,5 +2,5 @@ var twitch = require('./modules/twitch');
 require('./modules/logs');
 require('./modules/status');
 require('./modules/count');
-// require('./modules/combo');
+require('./modules/combo');
 
--- modules/combo.js
+++ modules/combo.js
@@ -10,15 +10,16 @@ var counter = 1;
 client.on('chat', function(channel, user, message, self) {
     var currentMessage = message;
 
-    if (currentMessage != lastMessage && counter > 3 && global.cooldown === false) {
-    	client.action(channel, counter + 'x ' + lastMessage + ' COMBO');
+    if (currentMessage != lastMessage) {
+    	if ( counter > 2 && global.cooldown === false) {
+    		client.action(channel, counter + 'x ' + lastMessage + ' COMBO');
+    	}
     	counter = 1;
     	global.cooldown = true;
     }
     if (currentMessage.toLowerCase() === lastMessage.toLowerCase()) {
     	counter++;
     }
-    lastMessage = message;
-    
-    
+    lastMessage = message; 
+    console.log(counter);
 });
