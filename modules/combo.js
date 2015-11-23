4504b03fc10e5664eca6e7e1ff4deba9b2cc567f
danielps1 <daniel.pasch.s@gmail.com>
2015-11-23 18:16:50 +0100

    combo fix

--- modules/combo.js
+++ modules/combo.js
@@ -12,7 +12,15 @@ client.on('chat', function(channel, user, message, self) {
 
     if (currentMessage != lastMessage) {
     	if ( counter > 2 && global.cooldown === false) {
-    		client.action(channel, counter + 'x ' + lastMessage + ' COMBO');
+            
+            if (fn.stringContainsUrl(searchPhrase) || fn.stringIsLongerThan(searchPhrase, 30)) {
+                var combo = 'the phrase';
+            }
+            else {
+                var combo = lastMessage;
+            }
+
+    		client.action(channel, counter + 'x ' + combo + ' COMBO');
             global.cooldown = true;
     	}
     	counter = 1;
