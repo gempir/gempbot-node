eb73a882ac6679f5c9031240b8a0d9ebc2b6c9a1
gempir <daniel.pasch.s@gmail.com>
2015-11-30 22:42:04 +0100

    whisper debugging

--- modules/twitch/whisper.js
+++ modules/twitch/whisper.js
@@ -2,10 +2,6 @@ var irc = require('tmi.js');
 var cfg = require('./../../cfg');
 
 var client = new irc.client({
-    connection: {
-        random: 'group',
-        reconnect: true
-    },
     identity: {
         username: cfg.options.username,
 		password: cfg.options.password
