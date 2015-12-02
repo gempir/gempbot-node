daa2f431388f3060ea47c0de5ccc1cf28c6c0a91
gempir <daniel.pasch.s@gmail.com>
2015-12-02 20:24:27 +0100

    better random quote logs

--- modules/quote.js
+++ modules/quote.js
@@ -10,7 +10,6 @@ function quoteCommandHandler(channel, user, message)
 	}
 	if (message.toLowerCase() !== '!randomquote') {
 		userToQuote = fn.getNthWord(message, 2);
-		console.log(userToQuote);
 		randomQuoteFromUser(channel, user, message, userToQuote);
 	}
 }
@@ -22,15 +21,14 @@ function randomQuoteFromChannel(channel, user, message)
 
 	fs.readFile('logs/' + channel.substr(1) + '.txt', function (err, data) {
 		if (err) {
-			console.log('randomquote channel fail');
+			console.log('[ERROR] randomquote channel fail');
 		}
     	
     	logsSplit = data.toString().split("\n");
-    	console.log(logsSplit[250]);        
         quote = logsSplit[Math.floor(Math.random()*logsSplit.length)];
 
         if (quote.length > 150 || fn.stringContainsUrl(quote)) {
-        	console.log('skipped channel quote');
+        	console.log('[LOG] skipped channel quote');
         	randomQuoteFromChannel(channel, user, message);
         	return false;
         }
@@ -42,24 +40,31 @@ function randomQuoteFromChannel(channel, user, message)
 function randomQuoteFromUser(channel, user, message, userToQuote) 
 {
 	userToQuote = userToQuote.toLowerCase();
-	fs.readFile('logs/' + channel.substr(1) + '/' + userToQuote + '.txt', function (err, data) {
-		if (err) {
-			output.say(userToQuote + ' has no logs here')
-		}
-		console.log('userToQuote: ' + userToQuote);
-    	logsSplit = data.toString().split("\n");      
-        quote = logsSplit[Math.floor(Math.random()*logsSplit.length)];
-        console.log('[QUOTE] : ' + quote);
-        quote = quote.split(']');
-        quote = quote[1];
+	userFile = 'logs/' + channel.substr(1) + '/' + userToQuote + '.txt';
+	fs.exists(userFile, function (exists) {
+        if(exists){
+            fs.readFile(userFile, function (err, data) {
+				if (err) {
+					output.say(userToQuote + ' has no logs here')
+				}
+		    	logsSplit = data.toString().split("\n");      
+		        quote = logsSplit[Math.floor(Math.random()*logsSplit.length)];
+		        quote = quote.split(']');
+		        quote = quote[1];
 
-        if (quote.length > 150 || fn.stringContainsUrl(quote)) {
-        	console.log('skipped user quote');
-        	randomQuoteFromUser(channel, user, message, userToQuote);
-        	return false;
+		        if (quote.length > 150 || fn.stringContainsUrl(quote)) {
+		        	console.log('[LOG] skipped user quote');
+		        	randomQuoteFromUser(channel, user, message, userToQuote);
+		        	return false;
+		        }
+		        output.say(channel, '"' + quote + ' "');
+		    });
+        } else {
+            console.log('[ERROR]' + userToQuote + ' has no logs')
         }
-        output.say(channel, '"' + quote + ' "');
     });
+	
+	
 }
 
 
