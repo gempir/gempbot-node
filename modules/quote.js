8e7186d86651dfe3a15d7a2645f2b5a66432872a
gempir <daniel.pasch.s@gmail.com>
2015-12-06 11:20:38 +0100

    attempting to fix logs

--- modules/quote.js
+++ modules/quote.js
@@ -9,40 +9,45 @@ function quoteUser(channel, user, message, whisper)
 	var userToQuote = fn.getNthWord(message, 2); 
 	userToQuote = userToQuote.toLowerCase();
 	userFile = 'logs/' + channel.substr(1) + '/' + userToQuote + '.txt';
-	fs.exists(userFile, function (exists) {
-        if (fn.getFilesizeInKilobytes(userFile) < 10) {
+	
+	if (!fn.fileExists(userFile)) {
+    	console.log('[ERROR] ' + userToQuote + ' has no logs');
+    	return false;
+    }
+    if (fn.getFilesizeInKilobytes(userFile) < 1) {
+    	console.log('[ERROR] ' + userToQuote + ' logs less than 1KB');
+		return false;
+	}
+
+    fs.readFile(userFile, function (err, data) {
+		if (err) {
 			return false;
 		}
-        if(exists){
-            fs.readFile(userFile, function (err, data) {
-				if (err) {
-					return false;
-				}
-		    	logsSplit = data.toString().split("\n");      
-		        quote = logsSplit[Math.floor(Math.random()*logsSplit.length)];
-		        quote = quote.split(']');
-		        quote = quote[1];
+    	logsSplit = data.toString().split("\n");      
+        quote = logsSplit[Math.floor(Math.random()*logsSplit.length)];
+        quote = quote.split(']');
+        quote = quote[1];
 
-		        if (quote.length < 10 || quote.length > 150 || fn.stringContainsUrl(quote)) {
-		        	global.quoteCounter++;
-		        	if (global.quoteCounter > 9) {
-		        		global.quoteCounter = 0;
-		        		return false;
-		        	}
-		        	console.log('[LOG] skipped user quote');
-		        	quoteUser(channel, user, message, whisper);
-		        	return false;
-		        }
-		        if (whisper) {
-		        	output.whisper(user, '"' + quote + ' "');
-		        }
-		        else {
-		        	output.say(channel, '"' + quote + ' "');
-		        }
-		    });
-        } else {
-            console.log('[ERROR] ' + userToQuote + ' has no logs')
+        if (quote.length < 10 || quote.length > 150 || fn.stringContainsUrl(quote)) {
+        	global.quoteCounter++;
+        	if (global.quoteCounter > 9) {
+        		global.quoteCounter = 0;
+        		return false;
+        	}
+        	console.log('[LOG] skipped user quote');
+        	quoteUser(channel, user, message, whisper);
+        	return false;
         }
+        if (whisper) {
+        	output.whisper(user, '"' + quote + ' "');
+        }
+        else {
+        	output.say(channel, '"' + quote + ' "');
+        }
+    });
+        
+            
+      
     });
 	
 	
