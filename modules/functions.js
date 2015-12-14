1a6825e9ecb55f67917a7d57cfef977350ab8b4b
gempir <daniel.pasch.s@gmail.com>
2015-12-14 23:30:28 +0100

    weighted average

--- modules/functions.js
+++ modules/functions.js
@@ -23,6 +23,29 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function weightedAverage(input) {
+    var weights = [];
+    var values = [];
+    var weighted_total = 0;
+    var total_weight = 0;;
+
+    if (input.length % 3 !== 0) {
+        throw new Error("Input array length is not a multiple of 3.");
+    }
+
+    for (var i = 0; i < input.length; i += 3) {
+        weights.push(input[i] * input[i + 1]);
+        values.push(input[i + 2]);
+    }
+
+    for (var i = 0; i < weights.length; i += 1) {
+        weighted_total += weights[i] * values[i];
+        total_weight += weights[i];
+    }
+
+    return weighted_total / total_weight;
+}
+
 function getLine(filename, line_no, callback) {
     var stream = fs.createReadStream(filename, {
       flags: 'r',
@@ -180,5 +203,6 @@ module.exports =
     secsToTime,
     getRandomInt,
     logStats,
-    getLine
+    getLine,
+    weightedAverage
 };
