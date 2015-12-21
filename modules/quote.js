b5455dfbb9b2e4460ac23d27b9806d2d02375291
gempir <daniel.pasch.s@gmail.com>
2015-12-21 07:17:34 +0100

    removed randomquote without user parameter because it caused too much crashes, will investigate

--- modules/quote.js
+++ modules/quote.js
@@ -7,7 +7,6 @@ global.quoteCounter = 0;
 function getQuote(channel, user, message, whisper) 
 {
 	if (message.toLowerCase() === '!randomquote') {
-		getRandomQuote(channel, user.username, message, whisper)
         return false;
 	}
 	var userToQuote = fn.getNthWord(message, 2); 
@@ -50,45 +49,6 @@ function getQuote(channel, user, message, whisper)
         }
     });	
 }
-
-
-function getRandomQuote(channel, username, message, whisper)
-{
-    fs.readdir('logs/' + channel.substr(1) + '/', function (err, files) {
-        if (err) throw err;
-
-        var answerLog = files[Math.floor(Math.random()*files.length)];
-
-        if (global.quoteSkip > 20) {
-            return false;
-        }
-
-        fs.readFile('logs/' + channel.substr(1) + '/' + answerLog, function (err, data) {
-            if (err) {
-                return false;
-            }
-            logsSplit = data.toString().split("\n");      
-            answer = logsSplit[Math.floor(Math.random()*logsSplit.length)];
-            answer = answer.split(']');
-            answer = answer[1];
-
-            if (answer.length > 100 || fn.stringContainsUrl(answer)) {
-                console.log('[LOG] skipped user answer');
-                getRandomQuote(channel, username, message, whisper);
-                global.quoteSkip += 1;
-                return false;
-            }
-            if (whisper) {
-                output.whisper(user, '"' + answer + ' "');
-            }
-            else {
-                output.say(channel, '@' + username + ' "' + answer + ' "');
-            }
-        });
-
-    });
-}
-
 
 module.exports = 
 {
