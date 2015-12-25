3d53ffe04417e3ed0434d173cb062788696cd80d
gempir <daniel.pasch.s@gmail.com>
2015-12-25 13:35:43 +0100

    fixed status

--- modules/status.js
+++ modules/status.js
@@ -4,16 +4,16 @@ var git    = require('git-rev-sync');
 var output = require('./twitch/output');
 
 
-function statusBot(channel, user, message, whisper)
+function statusBot(channel, username, message, whisper)
 {
 	var time = process.uptime();
 	var uptime = fn.secsToTime((time + ""));
 
 	if (whisper) {
-		output.whisper(user, 'Bot uptime: ' + uptime + ' | branch: ' + git.branch() + ' (' + git.short() + ')');
+		output.whisper(username, 'Bot uptime: ' + uptime + ' | branch: ' + git.branch() + ' (' + git.short() + ')');
 	}
     else {
-    	output.sayNoCD(channel, '@' + user['username'] + ', bot uptime: ' + uptime + ' | branch: ' + git.branch() + ' (' + git.short() + ')');
+    	output.sayNoCD(channel, '@' + username + ', bot uptime: ' + uptime + ' | branch: ' + git.branch() + ' (' + git.short() + ')');
     }   
 }
 
