82383e9272393cc0b6624f15c64a3785fced7ca1
gempir <daniel.pasch.s@gmail.com>
2016-01-09 14:36:38 +0100

    group server bugs

--- managers/boot.js
+++ managers/boot.js
@@ -18,7 +18,3 @@ channel.client.on("connected", function (address, port) {
 whisper.group.on("connected", function (address, port, err) {
 	console.log('[STARTUP] Connected to group servers on ' + address + ':' + port);
 });
-
-whisper.group.on("disconnected", function (reason) {
-    process.exit(); // restart bot when not connected to group servers, because whispers won't work otherwise
-});
