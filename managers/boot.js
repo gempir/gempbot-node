80fde3091d331bc09ab557fa413de19f96056a36
gempir <daniel.pasch.s@gmail.com>
2016-01-09 14:50:05 +0100

    group server bugs

--- managers/boot.js
+++ managers/boot.js
@@ -11,10 +11,14 @@ config.refreshTrusted();
 config.setCooldowns();
 
 channel.client.on("connected", function (address, port) {
-	output.say(cfg.options.channels[0], 'Bot starting | branch: ' + git.branch() + ' (' + git.short() + ')');
+	// output.say(cfg.options.channels[0], 'Bot starting | branch: ' + git.branch() + ' (' + git.short() + ')');
 	console.log('[STARTUP] Bot starting on ' + address + ':' + port + ' | branch: ' + git.branch() + ' (' + git.short() + ')');
 });
 
 whisper.group.on("connected", function (address, port, err) {
 	console.log('[STARTUP] Connected to group servers on ' + address + ':' + port);
+});
+
+whisper.group.on("disconnected", function (reason) {
+    process.exit(); // restart bot when not connected to group servers, because whispers won't work otherwise
 });
