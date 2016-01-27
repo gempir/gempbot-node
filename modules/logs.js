45cc3b19fffeaf85e8b177ec7635ad91c77a87a0
danielps1 <daniel.pasch.s@gmail.com>
2016-01-27 18:35:31 +0100

    removed channel from logs size since we dont monitor it anymore

--- modules/logs.js
+++ modules/logs.js
@@ -78,26 +78,14 @@ function logsSize(channel, username, message)
         console.log('[LOG] ' + name + ' has no log here');
     }
     else {
-        if (name === 'channel') {
-            var file = 'logs/' + channel.substr(1) + '.txt'
-            var fileSize = fn.getFilesizeInKilobytes(file).toFixed(0);
-            var extension = ' KB';
-            if (fileSize > 1000) {
-                fileSize = fn.getFilesizeInMegabytes(file).toFixed(2);
-                extension = ' MB';
-            }
-            output.say(channel, '@' + username + ', ' + 'log file for channel ' + channel.substr(1) + ' is ' + fileSize + extension);
+        var file = 'logs/' + channel.substr(1) + '/' + name +  '.txt'
+        var fileSize = fn.getFilesizeInKilobytes(file).toFixed(0);
+        var extension = ' KB';
+        if (fileSize > 1000) {
+            fileSize = fn.getFilesizeInMegabytes(file).toFixed(2);
+            extension = ' MB';
         }
-        else {
-            var file = 'logs/' + channel.substr(1) + '/' + name +  '.txt'
-            var fileSize = fn.getFilesizeInKilobytes(file).toFixed(0);
-            var extension = ' KB';
-            if (fileSize > 1000) {
-                fileSize = fn.getFilesizeInMegabytes(file).toFixed(2);
-                extension = ' MB';
-            }
-            output.say(channel, '@' + username + ', ' + 'log file for ' + name + ' is ' + fileSize + extension);
-        }
+        output.say(channel, '@' + username + ', ' + 'log file for ' + name + ' is ' + fileSize + extension);
     }
 }
 
