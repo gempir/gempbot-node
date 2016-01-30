7f0825ec23a7239e5f1be576c1126550b5053a13
danielps1 <daniel.pasch.s@gmail.com>
2016-01-30 16:39:55 +0100

    saves oddshots of the channel

---
+++ modules/oddshots.js
@@ -0,0 +1,20 @@
+var cfg    = require('../cfg.js');
+var fs     = require('graceful-fs');
+var moment = require('moment');
+var fn     = require('./functions');
+var output = require('./../connection/output');
+
+
+function saveChannelOddshots(channel, username, message)
+{
+    if (message.indexOf('oddshot.tv/shot/nymn-hs') > -1) {
+        var file = 'logs/' + channel.substr(1) + '/nymn-oddshots.txt';
+        fs.appendFile(file, '[GMT+1 ' + moment().utcOffset(60).format('D.M.YYYY H:mm:ss')  + '] ' + username + ': ' + message + '\n', function(){});
+    }
+}
+
+
+module.exports =
+{
+    saveChannelOddshots
+}
