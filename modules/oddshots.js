39885d3e9c97c6af1a00a0f913d2066b2791ff44
danielps1 <daniel.pasch.s@gmail.com>
2016-01-30 17:51:42 +0100

    ONLY WORKS FOR NYMNS CHANNEL - the same oddshot will not appear in the oddshot logs, working on supporting every channel

--- modules/oddshots.js
+++ modules/oddshots.js
@@ -7,10 +7,31 @@ var output = require('./../connection/output');
 
 function saveChannelOddshots(channel, username, message)
 {
-    if (message.indexOf('oddshot.tv/shot/nymn-hs') > -1) {
-        var file = 'logs/' + channel.substr(1) + '/oddshots.txt';
-        fs.appendFile(file, '[GMT+1 ' + moment().utcOffset(60).format('D.M.YYYY H:mm:ss')  + '] ' + username + ': ' + message + '\n', function(){});
+    var file = 'logs/' + channel.substr(1) + '/oddshots.txt';
+    if (!fn.fileExists(file)) {
+        fs.appendFile(file, '=== Oddshots for ' + channel + ' ===\n', function(){});
     }
+    else if (message.indexOf('http://oddshot.tv/shot/nymn-hs-') > -1) {
+        parseOddshots(channel, username, message);
+    }
+}
+
+function parseOddshots(channel, username, message)
+{
+    var file = 'logs/' + channel.substr(1) + '/oddshots.txt';
+    var regex = 'http://oddshot\.tv\/shot\/nymn-hs-';
+    var shot = message.match(regex)
+    var oddshot = message.substr(shot.index, shot.index + 39);
+
+    fs.readFile(file, function(err,data) {
+        if  (data.indexOf(oddshot) > -1) {
+            return false;
+        }
+        else {
+            fs.appendFile(file, '[GMT+1 ' + moment().utcOffset(60).format('D.M.YYYY H:mm:ss')  + '] ' + username + ': ' + message + '\n', function(){})
+        }
+    });
+    ;
 }
 
 
