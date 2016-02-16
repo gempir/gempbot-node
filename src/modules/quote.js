95048cff1903a792f76c31fb5af8308053a03162
danielps1 <daniel.pasch.s@gmail.com>
2016-02-16 17:51:04 +0100

    quotes get skipped a lot more before exit

--- src/modules/quote.js
+++ src/modules/quote.js
@@ -34,7 +34,7 @@ function getQuote(channel, username, message, callback)
         if (quote.length > 200 || fn.stringContainsUrl(quote)) {
 			console.log('[LOG] skipped user quote');
 			quoteSkip++;
-        	if (quoteSkip > 3) {
+        	if (quoteSkip > 10) {
         		quoteSkip = 0;
         		return false;
         	}
