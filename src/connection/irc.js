c354054bc9408ce816e37682973c212a56f9f5bb
danielps1 <daniel.pasch.s@gmail.com>
2016-03-10 21:45:41 +0100

    added logging back to outgoing messages

--- src/connection/irc.js
+++ src/connection/irc.js
@@ -154,7 +154,7 @@ function say(channel, message, action) {
         prefix = '/me ';
     }
     irc.socket.write('PRIVMSG ' + channel + ' :' + prefix + message +'\r\n');
-    console.log(channel + ' ' + prefix + message):
+    console.log(channel + ' ' + prefix + message);
 }
 
 
