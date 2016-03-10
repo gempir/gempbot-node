dd26038024a230b2f24960cae93cab5275c491db
danielps1 <daniel.pasch.s@gmail.com>
2016-03-10 21:29:17 +0100

    config crash prevention

--- src/modules/combo.js
+++ src/modules/combo.js
@@ -38,7 +38,7 @@ function count(channel, user, message)
         combos[channel]['combo']++;
     }
     else if (combos[channel]['combo'] > 4){
-        if (typeof config.config[channel].combos == 'undefined') {
+        if (config.config[channel] == 'undefined') {
             return false;
         }
         if (config.config[channel].combos == 1) {
