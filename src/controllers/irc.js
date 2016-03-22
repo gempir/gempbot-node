1a906cbc2d59b2221aa8563f8ed10e25e81fd744
danielps1 <daniel.pasch.s@gmail.com>
2016-03-22 12:31:28 +0100

    debugging

--- src/controllers/irc.js
+++ src/controllers/irc.js
@@ -123,7 +123,6 @@ function getTags(data) {
     if (tags['display-name'].trim() == '') {
         tags['display-name'] = cfg.irc.username;
     }
-    console.log(tags['display-name']);
     return tags;
 }
 
