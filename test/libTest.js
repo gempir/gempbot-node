661e531f9fe12fc8a190c554f318b902d0b5474f
danielps1 <daniel.pasch.s@gmail.com>
2016-04-02 14:28:28 +0200

    test modified

---
+++ test/libTest.js
@@ -0,0 +1,30 @@
+var assert = require('chai').assert
+var lib     = require('./../src/lib');
+
+
+describe('lib', function() {
+
+
+    describe('countProperties()', function () {
+        it('counts propereties of object', function () {
+            var obj = {
+                test1: 1,
+                test2: 2,
+                test3: 3
+            }
+            var count = lib.countProperties(obj);
+            assert.equal(count, 3);
+        });
+    });
+
+    describe('removeFromArray()', function () {
+        it('counts propereties of object', function () {
+            var arr  = [1,2,3];
+            var arr2 = [1,2,3];
+            lib.removeFromArray(arr2, arr[0]);
+            assert.equal(arr[1], arr2[0]);
+        });
+    });
+
+
+});
