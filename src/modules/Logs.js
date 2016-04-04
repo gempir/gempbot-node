0b79951981f571897347116af179638f0122be98
danielps1 <daniel.pasch.s@gmail.com>
2016-04-04 20:33:44 +0200

    better query for randomquotes

--- src/modules/Logs.js
+++ src/modules/Logs.js
@@ -12,7 +12,7 @@ export default class Logs
 
     getRandomquoteForUsername(channel, username, prefix)
     {
-        this.bot.mysql.query('SELECT message FROM chatlogs WHERE username = ? AND channel = ? ORDER BY RAND() LIMIT 100', [username, channel], (err, results) => {
+        this.bot.mysql.query('SELECT message FROM chatlogs WHERE username = ? AND channel = ? AND LENGTH(message) < 200 ORDER BY RAND() LIMIT 100', [username, channel], (err, results) => {
             if (err || results.length == 0) {
                 console.log(err, results);
                 return;
@@ -33,7 +33,7 @@ export default class Logs
 
     getRandomquote(channel, prefix)
     {
-        this.bot.mysql.query('SELECT username, message FROM chatlogs WHERE channel = ? ORDER BY RAND() LIMIT 100', [channel], (err, results) => {
+        this.bot.mysql.query('SELECT username, message FROM chatlogs WHERE channel = ? AND LENGTH(message) < 200 ORDER BY RAND() LIMIT 100', [channel], (err, results) => {
             if (err || results.length == 0) {
                 console.log(err, results);
                 return;
