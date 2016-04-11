9567c41ca901b97b96e5b538fd422247de80dad0
danielps1 <daniel.pasch.s@gmail.com>
2016-04-11 20:34:11 +0200

    new log table

--- src/Eventhub.js
+++ src/Eventhub.js
@@ -16,7 +16,6 @@ export default class Eventhub {
                 this.subAlert(channel, data.username, data.months);
                 break;
             case 'timeout':
-                this.bot.db.insertLogEntry(channel, 'timeout', data);
                 break;
             case 'ban':
                 break;
