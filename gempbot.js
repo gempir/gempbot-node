07b1e0a07d91aa6e06780b6f1d40759b3392a9a0
gempir <daniel.pasch.s@gmail.com>
2016-10-08 10:50:39 +0200

    catch uncaught exception and enable graceful reconnect

--- gempbot.js
+++ gempbot.js
@@ -1,5 +1,10 @@
+process.on('uncaughtException', function (err) {
+    console.log(err);
+});
+
 require('nice-console')(console);
 require('./src/overlay/overlay');
 import Bot from './src/Bot';
 
-var bot = new Bot;
+var bot = new Bot();
+
